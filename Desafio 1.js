22511624b116602d746af80cd632ba6631ccc25e
Matheus Togni <matheustogni64@gmail.com>
2024-07-15 19:54:35 -0300

    first commit

---
+++ Desafio 1.js
@@ -0,0 +1,29 @@
+let heroi = "Matheus"
+let xp = 1001
+let nivel
+
+if (xp < 1000) {
+    nivel = 'Ferro'
+    console.log('O Herói', heroi, 'é de nível', nivel)
+} else if (xp > 1000 && xp < 2000) {
+    nivel = 'Bronze'
+    console.log('O Herói', heroi, 'é de nível', nivel)
+} else if (xp > 5000 && xp < 6000) {
+    nivel = 'Prata'
+    console.log('O Herói', heroi, 'é de nível', nivel)
+} else if (xp > 6000 && xp < 7000) {
+    nivel = 'Ouro'
+    console.log('O Herói', heroi, 'é de nível', nivel)
+} else if (xp > 7000 && xp < 8000) {
+    nivel = 'Platina'
+    console.log('O Herói', heroi, 'é de nível', nivel)
+} else if (xp > 8000 && xp < 9000) {
+    nivel = 'Ascendente'
+    console.log('O Herói', heroi, 'é de nível', nivel)
+} else if (xp > 9000 && xp < 10000) {
+    nivel = 'Imortal'
+    console.log('O Herói', heroi, 'é de nível', nivel)
+} else if (xp > 10000) {
+    nivel = 'Radiante'
+    console.log('O Herói', heroi, 'é de nível', nivel)
+} 
